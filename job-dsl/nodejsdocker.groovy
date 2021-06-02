4afec543f96631276e7758b6cc5077cf9b01bfc7
Sidharth Bhasin <33648547+Sidharth13@users.noreply.github.com>
2021-06-03 02:03:12 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('Sidharth13/docker-nodejs-demo')
+            repositoryName('sidbhasin13/jenkins-docker')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
